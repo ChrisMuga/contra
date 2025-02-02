1488b11f3334957bad5246f47806faf0a9c54e9e
Chris Muga <ChrisMuga94@Gmail.com>
2025-02-02 17:35:14 +0300

    Read file in C

--- src/main.c
+++ src/main.c
@@ -1,5 +1,20 @@
-#import <stdio.h>
+#include <stdio.h>
 
 int main(){
-	printf("hello world\n");
+	FILE *file = fopen("input.txt", "r");
+
+	if (file == NULL){
+		return printf("Error!\n");
+	}
+
+	char input[10][3000];
+
+	int c = 0;
+
+	while(fgets(input[c], 3000, file)){
+		printf("%d -> %s", (c+1), input[c]);
+		c = c+ 1;
+	}
+
+	fclose(file);
 }
