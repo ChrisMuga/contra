046e14b98a867bc1696c53a738f44a1e750710e6
Chris Muga <chrismuga94@gmail.com>
2025-10-03 11:42:07 +0300

    close file

--- src/main.zig
+++ src/main.zig
@@ -9,7 +9,11 @@ pub fn main() !void {
 
     if(cwd.openFile("input.txt", fs.File.OpenFlags{ })) |file| {
         // TODO: Read file line by line
-        print("file -> {any}", .{file.stat()});
+        print("File found\n", .{});
+        print("file ->\n{!}\n", .{file.stat()});
+
+        // Clean up: Close file
+        file.close();
     } else |e| {
         print("Error opening file: {!}", .{e});
     }
