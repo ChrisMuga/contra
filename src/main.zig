dd854837eef0bad0f2ac13c6c14656b3c2fcdb0e
Chris Muga <chrismuga94@gmail.com>
2025-10-03 11:31:59 +0300

    use cwd to open file

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,16 @@
 const std = @import("std");
-const fs = std.fs;
 
+const fs = std.fs;
 const print = std.debug.print;
+const assert = std.debug.assert;
 
 pub fn main() !void {
-    const _file = fs.openFileAbsolute("./input.txt", fs.File.OpenFlags{ });
+    const cwd = fs.cwd();
+
+    if(cwd.openFile("input.txt", fs.File.OpenFlags{ })) |file| {
+        // TODO: Read file line by line
+        print("file -> {any}", .{file.stat()});
+    } else |e| {
+        print("Error opening file: {!}", .{e});
+    }
 }
