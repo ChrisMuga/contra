7b6a0cdc1ad7e44e4ac57bea9676f37e069c2119
Chris Muga <ChrisMuga94@Gmail.com>
2025-10-03 14:34:21 +0300

    Read file, line by line

--- src/main.zig
+++ src/main.zig
@@ -8,10 +8,14 @@ pub fn main() !void {
     const cwd = fs.cwd();
 
     if(cwd.openFile("input.txt", fs.File.OpenFlags{ })) |file| {
-        // TODO: Read file line by line
-        // NOTE: Can we use the size of the file, in this case 1615 bytes, to get a buffer from the heap and use that buffer to write to stdout?
-        print("File found\n", .{});
-        print("file ->\n{!}\n", .{file.stat()});
+        const stat = try file.stat();
+
+        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        const allocator = gpa.allocator();
+
+        const contents = try file.reader().readAllAlloc(allocator, stat.size+1);
+
+        print("{s}", .{contents});
 
         // Clean up: Close file
         file.close();
