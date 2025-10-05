ed377d701b24884c925e516c87e9a7632293f878
Chris Muga <ChrisMuga94@Gmail.com>
2025-10-05 20:02:33 +0300

    Additional comment with example

--- src/utils.zig
+++ src/utils.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 
-// `echo` mimics print("Hello World") in Python3
+/// `echo` mimics print("Hello World") in Python3
+/// Example usage:
+/// - echo("Hello World");
 pub fn echo(val: []const u8) void {
     std.debug.print("{s}\n", .{val});
 }
