0d5e204b7c95fbb2c09afbe55e580c9563a2cc6c
Chris Muga <chrismuga94@gmail.com>
2025-10-06 19:34:03 +0300

    tweak doc-comments for utils.echo

--- src/utils.zig
+++ src/utils.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 
-/// `echo` mimics `print("Hello World")` in Python3
-/// Example usage:
+/// # Implementation 
+/// - mimics `print("Hello World")` in Python3
+/// ## Example usage:
 /// - echo("Hello World");
 pub fn echo(val: []const u8) void {
     std.debug.print("{s}\n", .{val});
