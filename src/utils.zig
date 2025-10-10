747586e6f06835a1792eccc292fa4c78ac793f16
Chris Muga <ChrisMuga94@Gmail.com>
2025-10-10 14:41:15 +0300

    print any using utils.echo

--- src/utils.zig
+++ src/utils.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
 
-/// # Implementation 
+/// # Implementation
 /// - mimics `print("Hello World")` in Python3
 /// ## Example usage:
 /// - echo("Hello World");
 pub fn echo(val: []const u8) void {
-    std.debug.print("{s}\n", .{val});
+    std.debug.print("{any}\n", .{val});
 }
