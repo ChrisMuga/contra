3e9160986a15b9e9b8ab31dd2a94202c23b48205
Chris Muga <ChrisMuga94@Gmail.com>
2025-10-11 14:31:44 +0300

    More TODOs on the C implementation

--- src/main.c
+++ src/main.c
@@ -2,13 +2,18 @@
 
 
 // TODO: Work on memory allocation using malloc or alloc
+// TODO: Get positional arguments from flags, on main
+// 		- Use that to pass file path
 int main(){
-	FILE *file = fopen("input.txt", "r");
+	FILE *file = fopen("./samples/1mb-examplefile-com.txt", "r");
 
 	if (file == NULL){
 		return printf("Error!\n");
 	}
 
+	// TODO: This will not suffice to read a large file like 
+	// 		- ./samples/1mb-examplefile-com.txt
+	// 		- What strategies can we use?
 	char input[2048][3000];
 
 	int i = 0;
