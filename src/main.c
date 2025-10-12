92acbc5be15ad15c33f0c097dd4397e6cb2327db
Chris Muga <ChrisMuga94@Gmail.com>
2025-10-12 21:28:20 +0300

    initial implementation using buffer and fseek and ftell

--- src/main.c
+++ src/main.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 // TODO: Work on memory allocation using malloc or alloc
 // TODO: Get positional arguments from command-line, on main
 // 		- Use that to pass file path
 int main(){
 	FILE *file = fopen("./samples/1mb-examplefile-com.txt", "r");
+	// FILE *file = fopen("./samples/input.txt", "r");
 
 	if (file == NULL){
 		return printf("Error!\n");
 	}
 
-	// TODO: This will not suffice to read a large file like 
-	// 		- ./samples/1mb-examplefile-com.txt
-	// 		- What strategies can we use?
-	char input[2048][3000];
+	// TODO: Explain line for line to understand what's going on here...
+	// Get file size
+	fseek(file, 0L, SEEK_END);
+	long size = ftell(file);
+	printf("File size: %ld\n", size);
 
-	int i = 0;
+	char *buffer;
 
-	while(fgets(input[i], 3000, file)){
-		printf("%d:\t%s", (i+1), input[i]);
+	buffer = (char *)malloc(size+1);
+
+	fseek(file, 0L, SEEK_SET);
+
+	int i = 1;
+	while(fgets(buffer, size+1, file)){
+		printf("%d: \t %s", i, buffer);
 		i = i+ 1;
 	}
 
