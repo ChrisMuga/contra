1d505536694d6d8ddb9c073ff5fb4bcb402acc37
Chris Muga <ChrisMuga94@Gmail.com>
2025-10-15 11:06:47 +0300

    implement line number specification

--- src/main.c
+++ src/main.c
@@ -3,13 +3,26 @@
 
 // TODO: Implement piping e.g. `git log | contra`
 // TODO: Explain line for line to understand what's going on here...
+/// TODO: Show only line number, e.g `contra src/main.zig 14`
+/// TODO: Range of line numbers, e.g `contra src/main.zig 14-18`
 int main(int argc, char* argv[]){
 	if(argc <= 1) {
 		printf("Error: Please specify the input file\n");
 		return 0;
 	}
 
+	char* number;
+	int line_number = -1;
+
 	char* filename = argv[1];
+
+	if(argc > 1) {
+		number = argv[2];
+	}
+
+	if(number != NULL) {
+		line_number = atoi(number);
+	}
 
 	FILE *file = fopen(filename, "r");
 
@@ -20,7 +33,10 @@ int main(int argc, char* argv[]){
 	// Get file size
 	fseek(file, 0L, SEEK_END);
 	long size = ftell(file);
-	printf("File size: %ld\n", size);
+
+	if(line_number == -1) {
+		printf("File size: %ld\n", size);
+	}
 
 	char *buffer;
 
@@ -29,8 +45,15 @@ int main(int argc, char* argv[]){
 	fseek(file, 0L, SEEK_SET);
 
 	int i = 1;
+
 	while(fgets(buffer, size+1, file)){
-		printf("%d: \t %s", i, buffer);
+		if(line_number == -1) {
+			printf("%d: \t %s", i, buffer);
+		}else if(line_number == i){
+			printf("%d: \t %s", i, buffer);
+			break;
+		}
+
 		i = i+ 1;
 	}
 
