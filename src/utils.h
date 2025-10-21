c37a0f66f7689138981defaf5ff6596e2d628b71
Chris Muga <ChrisMuga94@Gmail.com>
2025-10-21 06:04:57 +0300

    split_range to obtain range integers

--- src/utils.h
+++ src/utils.h
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void echo(char val[]){
 	printf("%s\n", val);
+}
+
+// TODO: Documentation
+int* split_range(char range[]) {
+	// TODO; Is there a way to know the length of char[] without string.h?
+	static int x[2];
+
+	static char buffer[] = "";
+
+	int c = 0;
+
+	for(int i = 0; i < strlen(range); i++) {
+		if(range[i] == ':'){
+			x[0] = atoi(buffer);
+			c = 0;
+			continue;
+		}else{
+			buffer[c] = range[i];
+			c++;
+		}
+	}
+
+	x[1] = atoi(buffer);
+
+	return x;
 }
